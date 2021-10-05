4ed7d144c00214fcc21e51c8af0503b497f3431a
Molama <50421947+Molama@users.noreply.github.com>
2021-10-05 13:32:21 +0200

    formatting/unused imports removed (v0.0.0)

--- PostCommit.groovy
+++ PostCommit.groovy
@@ -1,13 +1,7 @@
 package org.opin.mergebot
 
 import groovy.json.JsonSlurper
-import groovy.transform.Field
-import java.util.logging.Logger
 
-@Field def configuration = new GroovyScriptEngine('.').with{loadScriptByName(args[1])}
-this.metaClass.mixin configuration
-
-@Field Logger logger =Logger.getLogger("PostCommit.class");
 String path=args[0]
 def input = new JsonSlurper().parse(new File(path), "UTF-8")
 return process(input)
